ddb4e1a4b97855da9a1b01740d49a95a035ffc9e
hsh1108 <hash1108@cml.snu.ac.kr>
2020-09-14 17:55:10 +0900

    add arch, model base

---
+++ model/model.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+class MODEL(nn.Module):
+    def __init__(self, args):
+        super(MODEL, self).__init__()
+
+    def forward(self):
+        pass
+
+
+    def train(self, args):
+        pass
