a49f3f15a8cbcb91efa25b61432761ac232de753
hsh1108 <hash1108@cml.snu.ac.kr>
2020-09-16 14:29:03 +0900

    saving image with torchvision

---
+++ tools/image/tv.py
@@ -0,0 +1,26 @@
+import torch
+from torch.autograd import Variable
+import torchvision
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+
+
+"""
+save image using torchvision.
+"""
+
+def test():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+
+    dataset_dir = './samples'
+    test_data = dsets.ImageFolder(dataset_dir, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=10, shuffle=True, num_workers=4)
+
+    test_img = Variable(iter(test_loader).next()[0], requires_grad=True)
+    pic = (test_img.data + 1) / 2.0
+    torchvision.utils.save_image(pic, 'sample_img.jpg', nrow=1)
+
+if __name__ == '__main__':
+    test()
